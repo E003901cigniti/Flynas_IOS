9df18b77ff18584cd4745ce13624cb4a46415a5a
indrajeet kumar <Indrajeet.kumar@cigniti.com>
2017-11-21 13:06:47 +0530

    initial commit

---
+++ workspace_Flynas2/Flynas_IOS/src/main/java/Flynas/Flynas_IOS/App.java
@@ -0,0 +1,13 @@
+package Flynas.Flynas_IOS;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
